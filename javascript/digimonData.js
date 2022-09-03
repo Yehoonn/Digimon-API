ec8d2423d4d275c60188921963c4036f51e5ff3f
Yehoon Kang <jes6208@naver.com>
2022-09-03 11:18:54 +0900

    code refactoring

--- javascript/digimonData.js
+++ javascript/digimonData.js
@@ -1,3 +1,20 @@
+// export default async function digimonData() {
+//   let dataObject = {
+//     nameArray: [],
+//     imgArray: [],
+//   };
+
+//   let response = await fetch("https://digimon-api.vercel.app/api/digimon");
+//   let result = response.json();
+//   return result.then((value) => {
+//     for (let key of value) {
+//       dataObject.nameArray.push(key.name);
+//       dataObject.imgArray.push(key.img);
+//     }
+//     return dataObject;
+//   });
+// }
+
 // export const digimonData = async () => {
 //   return await fetch("https://digimon-api.vercel.app/api/digimon")
 //     .then(async (res) => {
@@ -20,58 +37,12 @@
 //     });
 // };
 
-// export default async function digimonData() {
-//   let dataObject = {
-//     nameArray: [],
-//     imgArray: [],
-//   };
-
-//   let response = await fetch("https://digimon-api.vercel.app/api/digimon");
-//   let result = response.json();
-//   return result.then((value) => {
-//     for (let key of value) {
-//       dataObject.nameArray.push(key.name);
-//       dataObject.imgArray.push(key.img);
-//     }
-//     return dataObject;
-//   });
-// }
-
 const digimonData = async () => {
   return await fetch("https://digimon-api.vercel.app/api/digimon").then(
     async (res) => {
-      const data = await res.json();
-
-      return data.map((value) => {
-        return {
-          name: value.name,
-          img: value.img,
-        };
-      });
+      return await res.json();
     }
   );
 };
 
 export default digimonData;
-
-// const digimonData = async () => {
-//   return await fetch("https://digimon-api.vercel.app/api/digimon").then(
-//     async (res) => {
-//       let dataObject = {
-//         nameArray: [],
-//         imgArray: [],
-//       };
-
-//       const data = await res.json();
-
-//       for (let key of data) {
-//         dataObject.nameArray.push(key.name);
-//         dataObject.imgArray.push(key.img);
-//       }
-
-//       return dataObject;
-//     }
-//   );
-// };
-
-// export default digimonData;
